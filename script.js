5e55d82be31605b2ab7decbeeb4f76152119dfb7
amanurmaliya <amanurmaliya609@gmail.com>
2024-08-09 16:54:28 +0530

    final Project For Help The Needy.com

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ document.getElementsByClassName('btn1')[0].onclick = function() {
    document.getElementsByClassName('btn2')[0].onclick = ()=>{
     window.location.href = "https://en.wikipedia.org/wiki/Poverty_in_India#:~:text=Based%20on%202019's%20PPPs%20International,than%20%246.85%20per%20day%20in";
    }
+
+   document.getElementsByClassName('image-section')[0].onclick = function()
+   {
+    window.location.href = 'ngo.html';
+   }
